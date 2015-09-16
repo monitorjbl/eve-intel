f9edd547b8628c0fa41ecfbfa30e1c9ea89883c9
Taylor Jones <monitorjbl@gmail.com>
2015-09-15 23:29:40 -0400

    using CDN for dependencies

--- scripts/bash/static.sh
+++ scripts/bash/static.sh
@@ -3,4 +3,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 echo "Syncing static content"
-aws s3 sync $DIR/../../src/main/webapp/ s3://monitorjbl.io/eve-intel/
+aws s3 sync --exclude "*.DS_Store" $DIR/../../src/main/webapp/ s3://monitorjbl.io/eve-intel/
